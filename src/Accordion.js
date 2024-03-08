100b11baa1be8cc14b1ce513c0e4ee1afbae68c5
Ahmed Mustafa <Ahmedzakho925@gmail.com>
2024-03-08 15:32:20 +0300

    initial commit

---
+++ src/Accordion.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import AccordionItem from "./AccordionItem";
+
+export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="React Allows developers to:"
+        num={22}
+        key="React Allows developers to:"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
+    </div>
+  );
+}
---
+++ src/faqs-data.js
@@ -0,0 +1,14 @@
+export const faqs = [
+  {
+    title: "What is React JS?",
+    text: "React.js, commonly referred to as React, is an open-source JavaScript library used for building user interfaces (UIs) and single-page applications. Developed and maintained by Facebook, React was first released in 2013. React allows developers to create reusable UI components that can be composed to build complex UIs efficiently.",
+  },
+  {
+    title: "What is React used for?",
+    text: "Here are some common use cases for React.js: Web Applications, Single-Page Applications (SPAs), User Interfaces (UIs), Cross-Platform Development(react native), Real-Time Data Applications, Progressive Web Apps (PWAs)",
+  },
+  {
+    title: "what are React alternatives?",
+    text: "Several alternatives to React.js exist, each with its own set of features, advantages, and use cases. Some popular alternatives include: Vue.js, Angular, Svelte, Ember.js, Preact, Stencil.",
+  },
+];
